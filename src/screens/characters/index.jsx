917289723d195276cee56bdb883f01b0386da585
yoniaiz <yoni29396@gmail.com>
2021-11-30 19:02:54 +0200

    fix characters component

--- src/screens/characters/index.jsx
+++ src/screens/characters/index.jsx
@@ -1,12 +1,10 @@
-import Loader from "react-loader-spinner";
 import { CharacterCard, CharactersFilters } from "../../components";
 import Spinner from "../../components/Spinner";
-import { useFilters, useFetchCharacters } from "../../hooks";
+import { useFetchCharacters } from "../../hooks";
 import styles from "./characters.module.css";
 
 const Characters = () => {
   const { characters, status } = useFetchCharacters();
-  const { filteredCharacters, onFiltersApply } = useFilters(characters);
 
   if (status === "idle" || status === "loading") {
     return <Spinner />;
@@ -15,9 +13,8 @@ const Characters = () => {
   if (status === "success" && characters.length) {
     return (
       <div className={styles.charactersScreenContainer}>
-        <CharactersFilters onFiltersApply={onFiltersApply} />
         <div className={styles.charactersContainer}>
-          {filteredCharacters.map((character) => (
+          {characters.map((character) => (
             <CharacterCard key={character.id} {...character} />
           ))}
         </div>
