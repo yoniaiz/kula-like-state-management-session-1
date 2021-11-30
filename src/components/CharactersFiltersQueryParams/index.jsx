85dc818cce0c9682cce5c48cf11856a0b2561cf7
yoniaiz <yoni29396@gmail.com>
2021-11-30 19:52:06 +0200

    solution

--- src/components/CharactersFiltersQueryParams/index.jsx
+++ src/components/CharactersFiltersQueryParams/index.jsx
@@ -4,10 +4,10 @@ import styled from "./filters.module.css";
 const CharactersFiltersQueryParams = () => {
   const [filterState, setFiltersState] = useSearchParams();
 
-  const gender = "";
-  const name = "";
-  const species = "";
-  const status = "";
+  const gender = filterState.get("gender");
+  const name = filterState.get("name");
+  const species = filterState.get("species");
+  const status = filterState.get("status");
 
   const handleInputChange = (e) => {
     const params = {};
@@ -26,6 +26,11 @@ const CharactersFiltersQueryParams = () => {
     if (name) {
       params["name"] = name;
     }
+
+    setFiltersState(
+      { ...params, [e.target.name]: e.target.value },
+      { replace: true }
+    );
   };
 
   return (
--- src/hooks/useFiltersByParams.js
+++ src/hooks/useFiltersByParams.js
@@ -3,10 +3,10 @@ import { useSearchParams } from "react-router-dom";
 export const useFiltersByParams = (characters) => {
   const [filtersState] = useSearchParams();
 
-  const gender = "";
-  const name = "";
-  const species = "";
-  const status = "";
+  const gender = filtersState.get("gender");
+  const name = filtersState.get("name");
+  const species = filtersState.get("species");
+  const status = filtersState.get("status");
 
   let filteredCharacters = characters;
   if ((gender || name || species || status) && characters.length) {
